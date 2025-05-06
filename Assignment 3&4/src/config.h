db235441a7b3edc89b72010034fa95fa47f7b12a
Xuan Anh Khoa <165990708+xuananhkhoa@users.noreply.github.com>
2025-05-06 14:58:30 +0700

    Update config.h

--- Assignment 3&4/src/config.h
+++ Assignment 3&4/src/config.h
@@ -4,8 +4,8 @@
 using namespace std;
 
 
-const char* SSID = "ChoiSongJun";
-const char* PASSWORD = "qvuw9726";
+const char* SSID = "Khoa";
+const char* PASSWORD = "Khoa22122005";
 
 const string ID = "99fbca6d-21c3-4669-a466-aa9a41f16236";
 const string BROKER = "test.mosquitto.org";
